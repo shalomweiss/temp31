2b792ffd18f1fe675ba1eb6755e6655520ed51b0
shalomweiss <shalom.weiss@gmail.com>
2017-02-24 10:38:05 +0200

    Secound V
    ldhvifdoifd

--- DB/LibArabDB.sql
+++ DB/LibArabDB.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `libArabDB` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+﻿CREATE DATABASE  IF NOT EXISTS `libArabDB` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `libArabDB`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
@@ -11,45 +11,12 @@ USE `libArabDB`;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `bibliography`
---
-
-DROP TABLE IF EXISTS `bibliography`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bibliography` (
-  `idbibliographyID` int(11) NOT NULL DEFAULT '0',
-  `bibliographycolName` varchar(45) NOT NULL DEFAULT 'Default',
-  `userID` int(11) NOT NULL,
-  `WhenAdd` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idbibliographyID`,`userID`),
-  KEY `fk_bibliography_user1_idx` (`userID`),
-  CONSTRAINT `fk_bibliography_user1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
+/*!40103 SET TIME_ZONE='+00:00' */; FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bibliography`
---
-
-LOCK TABLES `bibliography` WRITE;
-/*!40000 ALTER TABLE `bibliography` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bibliography` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `favorite`
---
-
-DROP TABLE IF EXISTS `favorite`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- Dumping data for tabclient     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `favorite` (
   `userID` int(11) NOT NULL,
